5f8c7cb6708b24cc3cd70cd3cbf34537f63c9d37
Ckaii <tanwenkai2004@gmail.com>
2025-08-03 13:29:28 +0800

    Try Spline for 3d object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import SplineScene from "./components/splineScene";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <SplineScene />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ frontend/src/components/splineScene.jsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineScene() {
+	return (
+		<Spline scene="https://prod.spline.design/1K0diC315IPqRNf4/scene.splinecode" />
+	);
+}
+
